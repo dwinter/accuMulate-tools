a976a913704f970026507594e7a5d00c48fdd8b0
Abigail Howell <aahowel3@asu.edu>
2016-07-29 16:30:13 -0700

    included error messages in script close#1

--- dictionary_converter.py
+++ dictionary_converter.py
@@ -10,11 +10,22 @@ def main():
     Usage: 
         $python2 dictionary_converter.py [reference_genome.fasta]
     """  
+    try: 
+        file_input=sys.argv[1] 
+    except IndexError: 
+        print("Usage: dictionary_converter.py [file.fasta]")     
+        sys.exit(1) 
+
     with open(sys.argv[1]) as file_input:
         records=SeqIO.parse(file_input, "fasta")
-        fasta={x.id:len(x) for x in records} 
-        for k,v in fasta.iteritems():
-	    print k+"\t",v
+        fasta=[(x.id,len(x)) for x in records]
+        if len(fasta)==0:
+            raise RuntimeError("could not extract fasta records from file, check file format")
+            sys.exit(1)
+        for pair in fasta:
+            print "{}\t{}".format(*pair)
+        sys.exit(0)
+
     
 if __name__ == "__main__":
     main()
