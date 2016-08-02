0f4b218918cc48540de48f98bddb876c8d5767de
David Winter <david.winter@gmail.com>
2016-08-02 16:49:08 -0700

    Tweaks to the GC content script
     * Use numbered formatters (not just `{}`) to reduce repetition
     * Use only {} formatting, not a mix of % and {}
     * Removed no longer needed if statement
     * Added try -> except block

--- GC_content.py
+++ GC_content.py
@@ -2,7 +2,6 @@
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 import sys
-from collections import Counter 
 
 def GC_content(r):
     """calculates the GC content of a SeqRecord object (r) and multiples 
@@ -12,7 +11,7 @@ GC content was calculated proportional to each object and added to
 create the GC content of the entire sequence file produced by Bio 
 SeqIO.pars.
     """
-    fraction=GC(r.seq)/100
+    fraction=GC(r.seq)/100.0
     length=len(r.seq)
     GC_bases=fraction*length
     return(GC_bases)
@@ -25,21 +24,24 @@ total number of nucleotides in a SeqRecord object
     GC_bases=0
     bases=0
     for r in ref: 
-        if GC_content(r): 
-            GC_bases+=((GC(r.seq)/100)*(len(r.seq)))
-            bases+=len(r.seq)
+        nbase = len(r.seq)
+        GC_bases+=(GC(r.seq)/100)*nbase 
+        bases+=nbase
     return(GC_bases, bases) 
     
  
 def main():
-    filename=sys.argv[1]
+    try:
+        filename=sys.argv[1]
+    except IndexError:
+        print("Usage GC_content,py [reference genome (fasta)]")
+        exit(1)
     ref=SeqIO.parse(filename,"fasta")
     GC_bases, bases=base_counter(ref)
     GC_percentage=GC_bases/bases
     AT_percentage=1-GC_percentage
     #print(GC_percentage) 
-    print "nfreqs={}\t{}\t{}\t{}".format("%.3f" % AT_percentage, 
-"%.3f" % GC_percentage, "%.3f" % GC_percentage,"%.3f" % AT_percentage) 
+    print ("nfreqs={0:.3f} {1:.3f} {1:.3f} {0:.3f}".format(AT_percentage/2.0, GC_percentage/2.0))
    
   
 if __name__ == "__main__":
